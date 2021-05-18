d8b6a7c6f60bcb9a6c458b9b4937bc8f5897fbe6
Keuin <keuinx@gmail.com>
2021-05-18 21:02:03 +0800

    switch from Flask to FastAPI

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 import logging
 import time
 
-from flask import Flask, Response
-from flask import request as req
+from fastapi import FastAPI, Response
 
 from core.mux import MuxServer
 
 # from werkzeug.serving import WSGIRequestHandler
-
 # logging.basicConfig(filename='ym.log', filemode='a', level=logging.DEBUG)
+
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s][%(asctime)s][line:%(lineno)d] %(message)s')
 logging.info('YggdrasilMux is starting ...')
 logging.debug('DEBUG logging is enabled.')
 
 VERSION = "1.0-dev"
-app = Flask(__name__)
+app = FastAPI()
 mux = MuxServer([
     'https://sessionserver.mojang.com',
     'https://mcskin.littleservice.cn/api/yggdrasil/sessionserver'
 ])
 
 
-@app.route('/', methods=['GET'])
-def root():
+@app.get('/') # methods=['GET']
+async def root():
     logging.info('Root is called.')
     return {
         'mux_server': VERSION,
@@ -30,14 +29,14 @@ def root():
     }
 
 
-@app.route('/join', methods=['POST'])
-def join():
-    logging.info(f'join was called: {req.form}')
+# @app.route('/join', methods=['POST'])
+# def join():
+#     logging.info(f'join was called: {req.form}')
 
 
-@app.route('/sessionserver/session/minecraft/hasJoined', methods=['GET', 'POST'])
-def hasJoined():
-    args = req.args
+@app.get('/sessionserver/session/minecraft/hasJoined') # methods=['GET', 'POST']
+async def hasJoined(username, serverId):
+    args = {"username": username, "serverId": serverId}
     logging.debug(f'hasJoined is called. args={args}')
     # print(f'Received requests :{args}')
     json_str, status_code = mux.hasJoined(args)
@@ -49,25 +48,26 @@ def hasJoined():
 
     logging.info(f'Mux make response with statcode={status_code}: {json_str}')
     # return j, status_code
-    return Response(json_str, status=status_code, mimetype='application/json; charset=utf8')
+    # ; charset=utf8
+    return Response(json_str, status_code=status_code, media_type='application/json', headers={"Connection": "close"})
 
 
-@app.route('/api/profiles/minecraft', methods=['POST'])
-def query_profiles():
-    data = req.data
-    logging.debug(f'profiles is called. data={data}')
+# @app.route('/api/profiles/minecraft', methods=['POST'])
+# def query_profiles():
+#     # data = req.data
+#     logging.debug(f'profiles is called. data={data}')
 
     # json_str, status_code = mux.profile()
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    logging.error(f'Uncaught query path: {path}')
-    return 'You want path: %s' % path
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     logging.error(f'Uncaught query path: {path}')
+#     return 'You want path: %s' % path
 
 
 # TODO: Implement `hasJoined` API
 
-if __name__ == '__main__':
-    # WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    app.run()
+# if __name__ == '__main__':
+#     # WSGIRequestHandler.protocol_version = "HTTP/1.1"
+#     app.
